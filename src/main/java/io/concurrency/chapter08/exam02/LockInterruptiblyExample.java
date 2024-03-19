0393b03613941e0f84b88a646047a4f86af57c2f
ats3059 <ats3059@naver.com>
2024-03-19 23:52:12 +0900

    ReadWriteLock까지 학습

--- src/main/java/io/concurrency/chapter08/exam02/LockInterruptiblyExample.java
+++ src/main/java/io/concurrency/chapter08/exam02/LockInterruptiblyExample.java
@@ -4,21 +4,27 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class LockInterruptiblyExample {
+
     public static void main(String[] args) {
         Lock lock = new ReentrantLock();
 
         Thread thread1 = new Thread(() -> {
             try {
-                lock.lockInterruptibly(); // 락을 시도하며, 인터럽트가 들어오면 중단
+                lock.lockInterruptibly();
                 try {
-                    System.out.println("스레드 1이 락을 획득했습니다");
+                    Thread.sleep(3000);
+                    System.out.println("스레드 1 이 락을 획득했습니다.");
+                } catch (InterruptedException ex) {
+                    
                 } finally {
                     lock.unlock();
-                    System.out.println("스레드 1이 락을 해제했습니다");
+                    System.out.println("스레드 1 이 락을 해제했습니다");
                 }
             } catch (InterruptedException e) {
-                System.out.println("스레드 1이 인터럽트를 받았습니다");
+                System.out.println("스레드 1 이 인터럽트를 받았습니다");
             }
+
+
         });
 
         Thread thread2 = new Thread(() -> {
--- src/main/java/io/concurrency/chapter08/exam02/TryLockExample.java
+++ src/main/java/io/concurrency/chapter08/exam02/TryLockExample.java
@@ -4,58 +4,61 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class TryLockExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Lock lock = new ReentrantLock();
 
         Thread thread1 = new Thread(() -> {
-            boolean acquired = false;
-            while (!acquired) {
-                acquired = lock.tryLock();
-                if (acquired) {
+            boolean aquired = false;
+            while(!aquired){
+                aquired = lock.tryLock();
+                if(aquired){
+                    System.out.println("스레드 1 이 락을 획득했습니다.");
                     try {
-                        System.out.println("스레드 1이 락을 획득했습니다");
-                        Thread.sleep(2000); // 스레드 1이 잠시 락을 보유
+                        Thread.sleep(2000);
                     } catch (InterruptedException e) {
-                        e.printStackTrace();
+                        throw new RuntimeException(e);
                     } finally {
                         lock.unlock();
-                        System.out.println("스레드 1이 락을 해제했습니다");
+                        System.out.println("스레드 1 이 락을 해제했습니다.");
                     }
-                } else {
-                    System.out.println("스레드 1이 락을 획득하지 못했습니다. 잠시 대기합니다.");
+                }else{
+                    System.out.println("스레드 1 이 락을 획득하지 못했습니다.");
                     try {
-                        Thread.sleep(1000); // 1초 대기 후 다시 시도
+                        Thread.sleep(1000);
                     } catch (InterruptedException e) {
-
-                        e.printStackTrace();
+                        throw new RuntimeException(e);
                     }
                 }
             }
         });
+
 
         Thread thread2 = new Thread(() -> {
-            boolean acquired = false;
-            while (!acquired) {
-                acquired = lock.tryLock();
-                if (acquired) {
+            boolean aquired = false;
+            while(!aquired){
+                aquired = lock.tryLock();
+                if(aquired){
+                    System.out.println("스레드 2 이 락을 획득했습니다.");
                     try {
-                        System.out.println("스레드 2가 락을 획득했습니다");
-                    } finally {
+
+                    }finally {
                         lock.unlock();
-                        System.out.println("스레드 2가 락을 해제했습니다");
+                        System.out.println("스레드 2 이 락을 해제했습니다.");
                     }
-                } else {
-                    System.out.println("스레드 2가 락을 획득하지 못했습니다. 잠시 대기합니다.");
+                }else{
+                    System.out.println("스레드 2 이 락을 획득하지 못했습니다.");
                     try {
-                        Thread.sleep(1000); // 1초 대기 후 다시 시도
+                        Thread.sleep(1000);
                     } catch (InterruptedException e) {
-                        e.printStackTrace();
+                        throw new RuntimeException(e);
                     }
                 }
             }
         });
-
         thread1.start();
         thread2.start();
+
+        thread1.join();
+        thread2.join();
     }
 }
