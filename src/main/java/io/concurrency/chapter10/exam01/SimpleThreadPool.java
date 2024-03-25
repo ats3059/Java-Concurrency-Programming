7bfd8c332e5034bffa57fff33260d4fe937bfc85
ats3059 <ats3059@naver.com>
2024-03-25 23:53:35 +0900

    주석 추가

--- src/main/java/io/concurrency/chapter10/exam01/SimpleThreadPool.java
+++ src/main/java/io/concurrency/chapter10/exam01/SimpleThreadPool.java
@@ -26,6 +26,10 @@ public class SimpleThreadPool {
 
     public void submit(Runnable task) {
         if (!isShutdown) {
+            /*
+             taskQueue 내부에 작업을 넣고 현재 taskQueue의 인스턴스 락을 획득하기 위해서 대기중인
+             스레드들을 깨운다. 해당 작업을 위해 synchronized 키워드를 사용한다.
+             */
             synchronized (taskQueue){
                 taskQueue.offer(task);
                 taskQueue.notifyAll();
@@ -35,6 +39,7 @@ public class SimpleThreadPool {
 
     public void shutdown(){
         isShutdown = true;
+        // shutdown 호출 시 taskQueue에서 모든 스레드들을 깨우고 thread.join을 사용하여 모든 스레드들의 작업이 종료될때까지 기다린다 -> 스레드는 작업이 끝났기 때문에 종료.
         synchronized (taskQueue) {
             taskQueue.notifyAll();
         }
@@ -54,14 +59,17 @@ public class SimpleThreadPool {
             while (!isShutdown){
                 Runnable task;
                 synchronized (taskQueue) {
+                    // 만약 실행 할 작업이 없다면
                     while (taskQueue.isEmpty() && !isShutdown){
                         try {
+                            // taskQueue 인스턴스 모니터에서 조건이 변경될때까지 대기시킨다 -> 작업이 추가되기 전까지.
                             taskQueue.wait();
                         } catch (InterruptedException e) {
                             throw new RuntimeException(e);
                         }
                     }
                 }
+                // 잠깐 사이에 작업이 사라질 경우를 위해서 확인하는 작업
                 if(!taskQueue.isEmpty()){
                     task = taskQueue.poll();
                 }else{
